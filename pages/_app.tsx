37e471baa7782e48db168410b772611fc50c3551
Suraj Mallick <surajhustlermallick@gmail.com>
2024-01-22 22:24:21 +0530

    added react-icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+
+
+//its like the layout.tsx in app router(expectyou cant have metadata here)
+//the Component prop in Page Router is like the children of layout.tsx in app router
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
---
+++ pages/testing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index2 = () => {
+  return (
+    <div>testing Route Route</div>
+  )
+}
+
+export default index2
